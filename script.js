60df75ed31d7a9151b66a7eff264908532dc9ab4
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-06-29 17:26:35 -0300

    atualização

--- script.js
+++ script.js
@@ -8,47 +8,60 @@ btn.addEventListener('click', () => {
 
 let jogador = 'X';
 
-function verificarVencedor() {
-    const blocos = document.querySelectorAll('.bloco');
-    const combinacoes = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-        [0, 4, 8], [2, 4, 6]
-    ];
 
-    for (const combinacao of combinacoes) {
-        const [a, b, c] = combinacao;
-        if (blocos[a].innerHTML && 
-            blocos[a].innerHTML === blocos[b].innerHTML && 
-            blocos[a].innerHTML === blocos[c].innerHTML) {
-            setTimeout(() => {
-                alert(`Jogador ${blocos[a].innerHTML} venceu!`);
+function verificarVencedor(){
+    const bloco = document.querySelectorAll('.bloco');
+    const combinações = [
+                        [0,1,2],
+                        [3,4,5],
+                        [6,7,8],
+                        [0,3,6],
+                        [1,4,7],
+                        [2,5,8],
+                        [2,4,6],
+                        [0,4,8]
+                        ]
+    for(const combinação of combinações){
+        const [a, b, c] = combinação;
+        if(bloco[a].innerHTML != '' && bloco[a].innerHTML === bloco[b].innerHTML && bloco[a].innerHTML === bloco[c].innerHTML){
+            setTimeout(()=>{
+                alert(`O jogador ' ${bloco[a].innerHTML} ' venceu o jogo!`);
                 reiniciarJogo();
-            }, 100);
-            return;
+            },100);
         }
     }
 
-    if ([...blocos].every(bloco => bloco.innerHTML !== '')) {
-        setTimeout(() => {
+    if([...bloco].every(b => b.innerHTML !== '')){
+        setTimeout(()=>{
             alert('Empate!');
             reiniciarJogo();
-        }, 100);
+        },100);
+    }
+
+
+    
+}
+
+function jogar(célula){
+    if(célula.innerHTML === ''){
+        célula.innerHTML = jogador;
+        if(jogador === 'X'){
+            jogador = 'O';
+        }
+        else{
+            jogador = 'X';
+        }
+        verificarVencedor()
     }
 }
 
-function jogar(celula) {
-    if (celula.innerHTML === '') {
-        celula.innerHTML = jogador;
-        verificarVencedor();
-        jogador = jogador === 'X' ? 'O' : 'X';
-    }
-}
 
-function reiniciarJogo() {
-    const blocos = document.querySelectorAll('.bloco');
-    blocos.forEach(bloco => bloco.innerHTML = '');
-    jogador = 'X';
+
+function reiniciarJogo(){
+    const bloco = document.querySelectorAll('.bloco')
     btn.style.display = 'block';
     tbl.style.display = 'none';
+    jogador = 'X';
+    bloco.forEach(bloco => bloco.innerHTML = '');
+
 }
